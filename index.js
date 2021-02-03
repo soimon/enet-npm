7e114768a5a2c7ad324436ac220131acbcfeb2ae
soimon <soimon@soimon.com>
2021-02-03 15:15:41 +0100

    Changed to ES6 module exports

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var ENETModule = require("./build/enet.js");
+const ENETModule = require("./build/enet.js");
+const jsapi_ = ENETModule.jsapi;
 
-var jsapi_ = ENETModule.jsapi;
+export const Host = require("./lib/Host.js").Host;
+export const createServer = require("./lib/Host.js").createServer;
+export const createClient = require("./lib/Host.js").createClient;
+export const createServerFromSocket = require("./lib/Host.js").createServerFromSocket;
+export const Event = require("./lib/Event.js").Event;
+export const Address = require("./lib/Address.js").Address;
+export const Packet = require("./lib/Packet.js").Packet;
+export const Peer = require("./lib/Peer.js").Peer;
+export const Buffer = require("buffer").Buffer; //for use in chrome app when creating packets
+export const PACKET_FLAG = require("./lib/PACKET_FLAG.js").PACKET_FLAG;
+export const PEER_STATE = require("./lib/PEER_STATE.js").PEER_STATE;
 
-var enet = module.exports = {};
-
-enet.Host = require("./lib/Host.js").Host;
-enet.createServer = require("./lib/Host.js").createServer;
-enet.createClient = require("./lib/Host.js").createClient;
-enet.createServerFromSocket = require("./lib/Host.js").createServerFromSocket;
-enet.Event = require("./lib/Event.js").Event;
-enet.Address = require("./lib/Address.js").Address;
-enet.Packet = require("./lib/Packet.js").Packet;
-enet.Peer = require("./lib/Peer.js").Peer;
-enet.Buffer = require("buffer").Buffer; //for use in chrome app when creating packets
-enet.PACKET_FLAG = require("./lib/PACKET_FLAG.js").PACKET_FLAG;
-enet.PEER_STATE = require("./lib/PEER_STATE.js").PEER_STATE;
-
-enet.init = function (func) {
+export const init = function (func) {
 	var funcPointer = ENETModule["Runtime_addFunction"](function (host_ptr) {
-		var addr = new enet.Address(jsapi_.host_get_receivedAddress(host_ptr));
+		var addr = new Address(jsapi_.host_get_receivedAddress(host_ptr));
 		return func(addr.address(), addr.port());
 	});
 	jsapi_.init(funcPointer);
